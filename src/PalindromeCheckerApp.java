46d72fd24df7643e14cab8dcc163c0fed421fa9b
Faheem <faheemframes@gmail.com>
2026-03-02 11:19:53 +0530

    UC10: Added placeholder code

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
-﻿    // UC8: Linked List Based Palindrome Checker
+﻿    // UC10: Case-Insensitive & Space-Ignored Palindrome
     public class PalindromeCheckerApp {
         public static void main(String[] args) {
-            System.out.println("Running UC8: Linked List Based Palindrome Checker");
-            // TODO: Add Linked List Based Palindrome Checker code here
+            System.out.println("Running UC10: Case-Insensitive & Space-Ignored Palindrome");
+            // TODO: Add Case-Insensitive & Space-Ignored Palindrome code here
         }
     }
