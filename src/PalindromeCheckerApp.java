7df29dc3bf76cbe019543f9cd6f575722b884e0a
Faheem <faheemframes@gmail.com>
2026-03-02 11:19:48 +0530

    UC8: Added placeholder code

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
-﻿    // UC9: Recursive Palindrome Checker
+﻿    // UC8: Linked List Based Palindrome Checker
     public class PalindromeCheckerApp {
         public static void main(String[] args) {
-            System.out.println("Running UC9: Recursive Palindrome Checker");
-            // TODO: Add Recursive Palindrome Checker code here
+            System.out.println("Running UC8: Linked List Based Palindrome Checker");
+            // TODO: Add Linked List Based Palindrome Checker code here
         }
     }
