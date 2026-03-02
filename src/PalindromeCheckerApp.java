472de943db9fdf91090fb884b40f96aedec66ed8
Faheem <faheemframes@gmail.com>
2026-03-02 11:19:59 +0530

    UC11: Added placeholder code

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
-﻿    // UC10: Case-Insensitive & Space-Ignored Palindrome
+﻿    // UC11: Object-Oriented Palindrome Service
     public class PalindromeCheckerApp {
         public static void main(String[] args) {
-            System.out.println("Running UC10: Case-Insensitive & Space-Ignored Palindrome");
-            // TODO: Add Case-Insensitive & Space-Ignored Palindrome code here
+            System.out.println("Running UC11: Object-Oriented Palindrome Service");
+            // TODO: Add Object-Oriented Palindrome Service code here
         }
     }
